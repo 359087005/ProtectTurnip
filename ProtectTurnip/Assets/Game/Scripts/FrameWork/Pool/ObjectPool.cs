3c1ea35710c1807f3767b7f6bc6e17abcd9ca157
HY-201806121621\Administrator <359087005@qq.com>
2018-09-12 22:22:38 +0800

    对象池部分

---
+++ ProtectTurnip/Assets/Game/Scripts/FrameWork/Pool/ObjectPool.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool
+{
+    public string ResourcesDir = "";
+
+    Dictionary<string, SubPool> poolsDict = new Dictionary<string, SubPool>();
+
+    //去对象
+    public GameObject Spawn(string name)
+    {
+        SubPool pool = null;
+        if (!poolsDict.ContainsKey(name))
+        {
+            CreatNewPool(name);
+        }
+
+        pool = poolsDict[name];
+        return pool.Spawn();
+    }
+
+    void CreatNewPool(string name)
+    {
+        //预设路径
+        string path = "";
+        if (string.IsNullOrEmpty(ResourcesDir))
+        {
+            path = name;
+        }
+        else
+        {
+            path = ResourcesDir + "/" + name;
+        }
+        //加载预设
+        GameObject tmpPrefab = Resources.Load<GameObject>(path);
+
+        //创建对象池
+        SubPool tmpPool = new SubPool(tmpPrefab);
+
+        poolsDict.Add(name,tmpPool);
+    }
+
+    //回收对象
+
+    public void UnSpawn(GameObject go)
+    {
+        SubPool pool = null;
+        //for (int i = 0; i < poolsDict.Count; i++)
+        //{
+        //}
+
+        foreach (SubPool sp in poolsDict.Values)
+        {
+            if (sp.Contains(go))
+            {
+                pool = sp;
+                break;
+            }
+        }
+
+        pool.UnSpawn(go);
+    }
+
+    //回收所有对象
+
+    public void UnSpawnAll()
+    {
+        foreach (SubPool sp in poolsDict.Values)
+        {
+            sp.UnSpawnAll();
+        }
+    }
+}
---
+++ ProtectTurnip/Assets/Game/Scripts/FrameWork/Pool/SubPool.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubPool
+{
+    GameObject _prefab;
+
+    List<GameObject> _objectsList = new List<GameObject>();
+
+    public string Name
+    {
+        get {return _prefab.name;}
+    }
+
+    public SubPool(GameObject prefab)
+    {
+        this._prefab = prefab;
+    }
+
+    public GameObject Spawn()
+    {
+        GameObject tmp = null;
+
+        for (int i = 0; i < _objectsList.Count; i++)
+        {
+            if (!_objectsList[i].activeSelf)
+            {
+                tmp = _objectsList[i];
+                break;
+            }
+        }
+
+        if (tmp == null)
+        {
+            tmp = GameObject.Instantiate<GameObject>(_prefab);
+            _objectsList.Add(tmp);
+        }
+
+        tmp.SetActive(true);
+        tmp.SendMessage("OnSpawn",SendMessageOptions.DontRequireReceiver);
+
+        return tmp;
+    }
+
+    public void UnSpawn(GameObject go)
+    {
+        if (Contains(go))
+        {
+            go.SendMessage("OnUnSpawn", SendMessageOptions.DontRequireReceiver);
+            go.SetActive(false);
+        }
+    }
+
+    public void UnSpawnAll()
+    {
+        for (int i = 0; i < _objectsList.Count; i++)
+        {
+            if (_objectsList[i].activeSelf)
+            {
+                UnSpawn(_objectsList[i]);
+            }
+        }
+    }
+    public bool Contains(GameObject go)
+    {
+        return _objectsList.Contains(go);
+    }
+}
